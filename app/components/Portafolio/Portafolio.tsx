f3b8961aefca2c4bcb4e6a595ebe230ff504e134
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-09 17:07:06 -0500

    Portafolio

---
+++ app/components/Portafolio/Portafolio.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Portafolio.module.css";
+
+const Portafolio: React.FC = () => {
+    return (
+        <section className={styles.portafolio}>
+            <div className={styles.container}>
+            </div>
+        </section>
+    );
+};
+
+export default Portafolio;
