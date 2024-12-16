747cea4a7d03ae4fe25d3352a1436fa0ed1fc473
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-16 10:08:29 -0500

    Portafolio actualizado

--- app/components/Portafolio/Portafolio.tsx
+++ app/components/Portafolio/Portafolio.tsx
@@ -7,7 +7,13 @@ const Portafolio: React.FC = () => {
             <div className={styles.container}>
                 <h2 className={styles.titulo}>PORTAFOLIO</h2>
                 <div className={styles.contenido}>
-                    
+                    <div className={styles.textoWrapper}>
+                        <h3 className={styles.encabezado}>¡Hola! Me llamo Marvin Héctor Campos Deza</h3>
+                        <p className={styles.descripcion}>
+                            Soy un desarrollador apasionado con experiencia en la creación de aplicaciones web dinámicas y funcionales.
+                            Mi objetivo es combinar habilidades de frontend y backend para ofrecer soluciones innovadoras e impactantes.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
